5e0c39ee1f25fe3c3756ff9e3fd0f614ff886c33
freejacque <freejacque@gmail.com>
2014-10-01 14:10:23 -0400

    sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    redirect_to(user_path(user))
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to(login_path)
   end
 end
