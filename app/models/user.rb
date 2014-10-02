ee46a9edc314af175864b0efd5f7f56148017668
h4w5 <philip@h4w5.com>
2014-10-02 11:36:06 -0400

    add tests to user for password and helpers, and found an error to fix!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ActiveRecord::Base
   belongs_to :favorite_recipe, class_name: "Recipe"
+
   validates :name, :role, :email, presence: true
   validates :email, uniqueness: true
 
   has_secure_password
 
-  # here is a helper to check if a user is an admin!
+  # here is are helpers to check user roles...
   #
   def is_admin?
-    self.role =~ /patissier/
+     (self.role =~ /patissier/) == 0 ? true : false
+  end
+
+  def is_baker?
+    self.role == 'baker'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,29 @@ RSpec.describe User, :type => :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:role) }
   it { should validate_uniqueness_of(:email) }
+  it { should have_secure_password }
+
+  describe "role helpers" do
+    describe "#is_admin?" do
+      it "is true when the user has a role of 'patissiere'" do
+        user = User.create(name: "PJ", email: "pj@ga.co", role: "patissiere", password: "12345")
+        expect(user.is_admin?).to eq(true)
+      end
+      it "is false when the user doesn't have a role of 'patissiere'" do
+        user = User.create(name: "PJ", email: "pj@ga.co", role: "baker", password: "12345")
+        expect(user.is_admin?).to eq(false)
+      end
+    end
+
+    describe "#is_baker?" do
+      it "is true when the user has a role of 'baker'" do
+        user = User.create(name: "PJ", email: "pj@ga.co", role: "baker", password: "12345")
+        expect(user.is_baker?).to eq(true)
+      end
+      it "is false when the user doesn't have a role of 'baker'" do
+        user = User.create(name: "PJ", email: "pj@ga.co", role: "patissiere", password: "12345")
+        expect(user.is_baker?).to eq(false)
+      end
+    end
+  end
 end
